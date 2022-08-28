a50e7fe0e6cb0ced47ae9e87b3d8b77146ac835d
Rafael <rafaelratier@hotmail.com>
2022-08-28 19:28:12 -0300

    feat(ballot box): implementa lógica que recebe e converte para int a quantidade de eleitores

--- src/election-day/BallotBox.cs
+++ src/election-day/BallotBox.cs
@@ -1,25 +1,34 @@
 namespace election_day
 {
-    public class BallotBox
+  public class BallotBox
+  {
+    private int receivedOption1;
+    private int receivedOption2;
+    private int receivedOption3;
+    private int optionNull;
+
+    public int GetCountVoters()
     {
-        private int receivedOption1;
-        private int receivedOption2;
-        private int receivedOption3;
-        private int optionNull;
-        
-        public int GetCountVoters()
-        {
-            throw new NotImplementedException();
-        }
+      string? votersQuantityInput;
 
-        public void Start(int countVoters)
-        {
-           throw new NotImplementedException();
-        }
+      do
+      {
+        Console.Write("Informe o número de eleitores: ");
 
-        public void PrintResult()
-        {
-           throw new NotImplementedException();
-        }
+        votersQuantityInput = Console.ReadLine();
+      } while (String.IsNullOrWhiteSpace(votersQuantityInput));
+
+      return Convert.ToInt32(votersQuantityInput);
     }
+
+    public void Start(int countVoters)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void PrintResult()
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
