3dff287fcdb915e0f25443604fedd2641f02f2a3
Rafael <rafaelratier@hotmail.com>
2022-08-28 20:36:41 -0300

    test(testthirdreq): implementa teste para resultados da votação

--- src/election-day.Test/TestThirdReq.cs
+++ src/election-day.Test/TestThirdReq.cs
@@ -9,15 +9,36 @@ namespace election_day.Test;
 [Collection("Sequential")]
 public class TestThirdReq
 {
-    [Theory(DisplayName = "Deve imprimir o resultado")]
-    [InlineData(new string[]{"6","1","1","5","A","3","2"}, 2, 1, 1, 2)]
-    public void TestPrintResult(
-        string[] entrys,
-        int expectedReceivedOption1,
-        int expectedReceivedOption2,
-        int expectedReceivedOption3,
-        int expectedOptionNull)
+  [Theory(DisplayName = "Deve imprimir o resultado")]
+  [InlineData(new string[] { "6", "1", "1", "5", "A", "3", "2" }, 2, 1, 1, 2)]
+  public void TestPrintResult(
+      string[] entries,
+      int expectedReceivedOption1,
+      int expectedReceivedOption2,
+      int expectedReceivedOption3,
+      int expectedOptionNull)
+  {
+    using (var stringWriter = new StringWriter())
     {
-        throw new NotImplementedException();
+      using (var stringReader = new StringReader(string.Join("\n", entries)))
+      {
+        Console.SetOut(stringWriter);
+        Console.SetIn(stringReader);
+
+        if (entries.Length <= 0)
+        {
+          throw new Xunit.Sdk.XunitException();
+        }
+
+        Program.Main();
+
+        var ballotResults = stringWriter.ToString().Trim();
+
+        ballotResults.Should().Contain($"A opção 1 recebeu: {expectedReceivedOption1} votos(s)");
+        ballotResults.Should().Contain($"A opção 2 recebeu: {expectedReceivedOption2} votos(s)");
+        ballotResults.Should().Contain($"A opção 3 recebeu: {expectedReceivedOption3} votos(s)");
+        ballotResults.Should().Contain($"Total de votos anulados: {expectedOptionNull} votos(s)");
+      }
     }
+  }
 }
